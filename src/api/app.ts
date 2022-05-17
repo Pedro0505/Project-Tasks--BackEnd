0f476c2c5a334ce29355435072f6acaa36824548
Pedro Henrique <pedrohrds05@hotmail.com>
2022-05-17 17:49:42 -0300

    feat: add a app to server

---
+++ src/api/app.ts
@@ -0,0 +1,24 @@
+import express from 'express';
+import cors from 'cors';
+
+class App {
+  public app: express.Express;
+
+  constructor() {
+    this.app = express();
+    this.middlewares();
+  }
+
+  private middlewares() {
+    this.app.use(express.json());
+    this.app.use(cors({ origin: '*', methods: ['GET', 'POST', 'DELETE', 'PUT', 'OPTIONS', 'PATCH'] }));
+  }
+
+  public start(PORT: string | number):void {
+    this.app.listen(PORT, () => console.log(`Server listen in port: ${PORT}`));
+  }
+}
+
+export { App };
+
+export const { app } = new App();
