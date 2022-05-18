ce032ac3f3fe25332ac5d8746711e5063e5b1fdd
Pedro Henrique <pedrohrds05@hotmail.com>
2022-05-18 15:02:47 -0300

    test(seeders): add seeders to tests

---
+++ __tests__/seeders/tasks.ts
@@ -0,0 +1,37 @@
+export const tasks = [
+  {
+    id: '8c96ea7c-ff45-4c25-8a36-123d3c254bfc',
+    content: 'Fazer projeto Store Manager',
+    tasksStatus: 'IN_PROGRESS',
+  },
+  {
+    id: '0f7446c3-43a1-4e49-b3ce-443bc3b81d2f',
+    content: 'Fazer projeto TFC',
+    tasksStatus: 'DONE',
+  },
+  {
+    id: '9812ff40-6053-49e3-b604-0bcd74648dd9',
+    content: 'Fazer projeto Zoo Functions',
+    tasksStatus: 'PEDDING',
+  },
+  {
+    id: 'a7f00f69-161f-431b-bb31-32349bf6b967',
+    content: 'Fazer projeto Shopping Cart',
+    tasksStatus: 'PEDDING',
+  },
+  {
+    id: 'b627e88f-9393-4a74-9839-89e22a621059',
+    content: 'Fazer projeto Blogs Api',
+    tasksStatus: 'DONE',
+  },
+  {
+    id: 'ddd6212d-5f03-4bbd-be3b-f1ca6de0ccf9',
+    content: 'Fazer projeto TrybeSmith',
+    tasksStatus: 'IN_PROGRESS',
+  },
+  {
+    id: '308978c7-074e-43a8-b02c-6456fcdc7fb7',
+    content: 'Fazer projeto TrybeTunes',
+    tasksStatus: 'PEDDING',
+  },
+];
