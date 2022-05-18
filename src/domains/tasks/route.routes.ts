80053631f6e3c595dae75dbab24c82e444caf6e2
Pedro Henrique <pedrohrds05@hotmail.com>
2022-05-17 21:27:06 -0300

    feat: add a routes base to tasks

---
+++ src/domains/tasks/route.routes.ts
@@ -0,0 +1,15 @@
+import { Router } from 'express';
+import TasksController from './controller';
+
+class TasksRoutes {
+  private _controller: TasksController;
+
+  private _route: Router;
+
+  constructor(router: Router, controller: TasksController) {
+    this._controller = controller;
+    this._route = router;
+  }
+}
+
+export default TasksRoutes;
