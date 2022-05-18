72f79bd326eccc62e18583ae1bf07f19c35f4a95
Pedro Henrique <pedrohrds05@hotmail.com>
2022-05-18 11:57:58 -0300

    feat(repository): add a repository to task route

--- src/domains/tasks/repository.ts
+++ src/domains/tasks/repository.ts
@@ -1,10 +1,52 @@
 import { PrismaClient } from '@prisma/client';
+import { ITaskCreateParams } from './interfaces/ICreateTask';
+import { ITaskRepository, IUpdateContent, IUpdateStatus } from './interfaces/ITaskRepository';
 
-class TasksRepository {
+class TasksRepository implements ITaskRepository {
   private _prisma: PrismaClient;
 
   constructor(prisma: PrismaClient) {
     this._prisma = prisma;
+  }
+
+  public async getAll() {
+    return await this._prisma.tasks.findMany();
+  }
+
+  public async create({ content, status }: ITaskCreateParams) {
+    const created = await this._prisma.tasks.create({ data: { content, tasksStatus: status } });
+
+    return {
+      id: created.id,
+      content,
+      status,
+    };
+  }
+
+  public async deleteTask(id: string) {
+    this._prisma.tasks.delete({ where: { id } });
+  }
+
+  public async updateStatus({ status, id }: IUpdateStatus) {
+    const updated = await this._prisma.tasks.update({
+      data: { tasksStatus: status },
+      where: { id },
+    });
+
+    return { id: updated.id, status: updated.tasksStatus };
+  }
+
+  public async updateContent({ content, id }: IUpdateContent) {
+    const updated = await this._prisma.tasks.update({ data: { content }, where: { id } });
+
+    return {
+      id: updated.id,
+      content: updated.content,
+    };
+  }
+
+  public async getTaskById(id: string) {
+    return await this._prisma.tasks.findUnique({ where: { id } });
   }
 }
 
