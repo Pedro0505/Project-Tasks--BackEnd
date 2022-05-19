7c0cb1f630cb109f9405f9092f3a88f34bbaf2cf
Pedro Henrique <pedrohrds05@hotmail.com>
2022-05-19 08:32:47 -0300

    feat(service): add a service create

--- src/domains/tasks/service.ts
+++ src/domains/tasks/service.ts
@@ -1,7 +1,10 @@
 import { Ok } from '../http-responses';
+import { Create } from '../http-responses/Create';
+import { ITaskCreateParams } from './interfaces/ICreateTask';
+import { ITaskService } from './interfaces/ITaskService';
 import TasksRepository from './repository';
 
-class TasksService {
+class TasksService implements ITaskService {
   private _repository: TasksRepository;
 
   constructor(repository: TasksRepository) {
@@ -12,6 +15,12 @@ class TasksService {
     const allTasks = await this._repository.getAll();
 
     return new Ok(allTasks);
+  }
+
+  public async create({ content, status }: ITaskCreateParams) {
+    const created = await this._repository.create({ content, status });
+
+    return new Create(created);
   }
 }
 
