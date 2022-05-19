49cf4bd34cef1c617578e416de7a51c7bddacce1
Pedro Henrique <pedrohrds05@hotmail.com>
2022-05-19 17:24:25 -0300

    feat(joi): add max length rule in joi schema content

--- src/domains/tasks/taskSchema.ts
+++ src/domains/tasks/taskSchema.ts
@@ -1,5 +1,4 @@
 import joi from 'joi';
-import { ITaskCreateParams } from './interfaces/ICreateTask';
 
 export default class TaskSchema {
   private joi: joi.Root;
@@ -9,12 +8,13 @@ export default class TaskSchema {
   }
 
   public createTask() {
-    return this.joi.object<ITaskCreateParams>({
-      content: this.joi.string().required().min(2).messages({
-        'any.required': '400|All fields must be filled',
-        'string.empty': '400|"Content" fild not be empty',
-        'string.base': '400|"Content" must be a string',
-      }),
+    return this.joi.object({
+      content: this.joi.string().required().min(2).max(50)
+        .messages({
+          'any.required': '400|All fields must be filled',
+          'string.empty': '400|"Content" fild not be empty',
+          'string.base': '400|"Content" must be a string',
+        }),
       status: this.joi.string().required().messages({
         'any.required': '400|All fields must be filled',
         'string.empty': '400|"Status" fild not be empty',
@@ -25,11 +25,12 @@ export default class TaskSchema {
 
   public updateContent() {
     return this.joi.object({
-      content: this.joi.string().required().min(2).messages({
-        'any.required': '400|All fields must be filled',
-        'string.empty': '400|"Content" fild not be empty',
-        'string.base': '400|"Content" must be a string',
-      }),
+      content: this.joi.string().required().min(2).max(50)
+        .messages({
+          'any.required': '400|All fields must be filled',
+          'string.empty': '400|"Content" fild not be empty',
+          'string.base': '400|"Content" must be a string',
+        }),
     });
   }
 }
