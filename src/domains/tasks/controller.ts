832080c7fe872f78c57e859b6ca3b4da1181a041
Pedro Henrique <pedrohrds05@hotmail.com>
2022-05-19 08:35:05 -0300

    feat(controller): add a controller create

--- src/domains/tasks/controller.ts
+++ src/domains/tasks/controller.ts
@@ -1,7 +1,8 @@
 import { Request, Response } from 'express';
+import { ITaskController } from './interfaces/ITaskController';
 import TasksService from './service';
 
-class TasksController {
+class TasksController implements ITaskController {
   private _service: TasksService;
 
   constructor(service: TasksService) {
@@ -10,6 +11,14 @@ class TasksController {
 
   public getAll = async (req: Request, res: Response) => {
     const { reponse: { code, data } } = await this._service.getAll();
+
+    return res.status(code).json({ data });
+  };
+
+  public create = async (req: Request, res: Response) => {
+    const { content, status } = req.body;
+
+    const { reponse: { code, data } } = await this._service.create({ content, status });
 
     return res.status(code).json({ data });
   };
