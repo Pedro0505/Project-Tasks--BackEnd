69fea3ce3424afe0738884cc3535fafab471dab9
Pedro Henrique <pedrohrds05@hotmail.com>
2022-05-19 17:23:33 -0300

    test(integration): add test to PATCH /tasks/contet/:id

--- __tests__/__mocks__/tasks.ts
+++ __tests__/__mocks__/tasks.ts
@@ -30,9 +30,21 @@ export const updatedContent = {
   request: { content: "Refazendo o ultimo requisito" },
   requestErroEmptyStr: { content: "" },
   requestErroMin: { content: "a" },
+  requestErroMax: { content: "a".repeat(51) },
   requestErroNotStr: { content: 1 },
   response: {
     id: '0f7446c3-43a1-4e49-b3ce-443bc3b81d2f',
     content: 'Refazendo o ultimo requisito'
   },
+  responseErrorNotFound: {
+    data: {
+      message: {
+        error: 'Task Not Found'
+      }
+    }
+  },
+  responseErroEmptyStr: { message: '\"Content\" fild not be empty' },
+  responseErroNotStr: {	message: '\"Content\" must be a string'  },
+  responseErroMin: { message: '\"content\" length must be at least 2 characters long' },
+  responseErroMax: { message: '\"content\" length must be less than or equal to 50 characters long' },
 }
--- __tests__/integration/task.test.ts
+++ __tests__/integration/task.test.ts
@@ -63,4 +63,75 @@ describe('Testando a rota /tasks', () => {
       expect(body).toStrictEqual({ data: { message: { error: 'Task Not Found' } } });
     });
   });
+
+  describe('Testando o PATCH /tasks/content/:id', () => {
+    beforeAll(async () => {
+      await prisma.tasks.createMany({ data: tasks });
+    });
+
+    afterAll(async () => {
+      await prisma.tasks.deleteMany();
+
+      await prisma.$disconnect();
+    });
+
+    it('Testando o updatedContent das tasks quando dá tudo certo', async () => {
+      const { body, status } = await request(app)
+      .patch('/tasks/content/0f7446c3-43a1-4e49-b3ce-443bc3b81d2f')
+      .send(mock.updatedContent.request);
+      
+      expect(status).toBe(200);
+      expect(body.data).toBeDefined();
+      expect(body.data).toStrictEqual(mock.updatedContent.response);
+    });
+
+    describe('Testando caso de erros do updatedContent', () => {
+      it('Testando o updatedContent das tasks quando o content vem vazio', async () => {
+        const { body, status } = await request(app)
+        .patch('/tasks/content/0f7446c3-43a1-4e49-b3ce-443bc3b81d2f')
+        .send(mock.updatedContent.requestErroEmptyStr);
+        
+        expect(status).toBe(400);
+        expect(body).toStrictEqual(mock.updatedContent.responseErroEmptyStr);
+      });
+
+      it('Testando o updatedContent das tasks quando o content so tem um caracter', async () => {
+        const { body, status } = await request(app)
+        .patch('/tasks/content/0f7446c3-43a1-4e49-b3ce-443bc3b81d2f')
+        .send(mock.updatedContent.requestErroMin);
+        
+        expect(status).toBe(400);
+        expect(body).toStrictEqual(mock.updatedContent.responseErroMin);
+      });
+
+      it('Testando o updatedContent das tasks quando o content tem mais de 50 caracteres', async () => {
+        const { body, status } = await request(app)
+        .patch('/tasks/content/0f7446c3-43a1-4e49-b3ce-443bc3b81d2f')
+        .send(mock.updatedContent.requestErroMax);
+
+        console.log(mock.updatedContent.requestErroMax)
+        
+        expect(status).toBe(400);
+        expect(body).toStrictEqual(mock.updatedContent.responseErroMax);
+      });
+
+      it('Testando o updatedContent das tasks quando o content não é um numero', async () => {
+        const { body, status } = await request(app)
+        .patch('/tasks/content/0f7446c3-43a1-4e49-b3ce-443bc3b81d2f')
+        .send(mock.updatedContent.requestErroNotStr);
+        
+        expect(status).toBe(400);
+        expect(body).toStrictEqual(mock.updatedContent.responseErroNotStr);
+      });
+
+      it('Testando o updatedContent das tasks quando não tem o id encontrado', async () => {
+        const { body, status } = await request(app)
+        .patch('/tasks/content/notHaveId')
+        .send(mock.updatedContent.request);
+        
+        expect(status).toBe(404);
+        expect(body).toStrictEqual(mock.updatedContent.responseErrorNotFound);
+      });
+    })
+  });
 });
